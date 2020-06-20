4509836da6dd153023359037a997055c7f4ac1ec
BalkrishnaBhatt <bhattbalkrishna7@gmail.com>
2020-06-21 01:34:51 +0530

    final

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -31,7 +31,7 @@ class Recipe extends Component{
                         <li className="collection-item"><b>Total: {this.props.total} $</b></li>
                     </div>
                     <div className="checkout">
-                        <button className="waves-effect waves-light btn">Checkout</button>
+                        <button disabled={this.props.total === 0}  className="waves-effect waves-light btn">Checkout</button>
                     </div>
                  </div>
         )
